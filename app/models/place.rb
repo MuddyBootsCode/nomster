28fac80ec394d148071281dbded0e869dc56bbe1
Michael Porter <firsttogo98@gmail.com>
2017-05-17 05:00:01 +0000

    added form validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user 
-  validates :name, presence: true
+  validates :name, :description, :address,  presence: true
+  validates :name, length: { minimum: 3}
+  validates :name, :description, :address, exclusion: { in: %w(www com @ # io),
+    message: "%{value} is reserved."}
+  
+
 end
