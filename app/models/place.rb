15e2918b28e90ed78651fa2c34ae4b81c7d48f61
Michael Porter <firsttogo98@gmail.com>
2017-05-17 06:26:40 +0000

    Added API Keys and Figaro

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,11 @@ class Place < ApplicationRecord
   belongs_to :user 
   validates :name, :description, :address,  presence: true
   validates :name, length: { minimum: 3}
-  validates :name, :description, :address, exclusion: { in: %w(www com @ # io),
+  validates :name, :description, :address, exclusion: { in: %w(www com @ # /\B#\w+/),
     message: "%{value} is reserved."}
+
+  geocoded_by :address
+  after_validation :geocode
   
 
 end
