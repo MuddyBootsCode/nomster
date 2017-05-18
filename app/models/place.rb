949ecb420d348b0ad1d96d825a2e9609939ea513
Michael Porter <firsttogo98@gmail.com>
2017-05-18 02:14:57 +0000

    added comments to db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,13 @@
 class Place < ApplicationRecord
+  
   belongs_to :user 
+  has_many :comments 
+
   validates :name, :description, :address,  presence: true
   validates :name, length: { minimum: 3}
-  validates :name, :description, :address, exclusion: { in: %w(www com @ # /\B#\w+/),
-    message: "%{value} is reserved."}
+  
 
   geocoded_by :address
   after_validation :geocode
   
-
 end
